7b2a3e87f10b2c96122d7c9c3cee156316faaceb
Sergio Gonzalez Barraza <114702763+SergioGzzBrz@users.noreply.github.com>
2025-03-11 10:24:00 -0600

    Add lost files on commit for firebase

---
+++ routes/items3.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express"
+import { getItems, getItem, postItem, putItem, deleteItem } from "../controllers/items3.controller.js";
+
+const router = Router();
+
+router.get("/items3/", getItems);
+router.get("/items3/:id/", getItem);
+router.post("/items3/", postItem);
+router.put("/items3/:id/", putItem);
+router.delete("/items3/:id/", deleteItem);
+
+export default router;
