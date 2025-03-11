68bfddc009aaa64b1b393f64fc5f61b4a14eca93
Sergio Gonzalez Barraza <114702763+SergioGzzBrz@users.noreply.github.com>
2025-03-11 10:22:27 -0600

    Add firebase connections

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import morgan from 'morgan'
 import indexRouter from "./routes/index.routes.js"
 import itemsRouter from './routes/items.routes.js'
 import items2Router from './routes/items2.routes.js'
+import items3Router from './routes/items3.routes.js'
 import loginRouter from './routes/login.routes.js'
 import { connectMongo } from './utils/mongodb.js'
 
@@ -20,5 +21,6 @@ app.use(indexRouter)
 app.use(itemsRouter)
 app.use(loginRouter)
 app.use(items2Router)
+app.use(items3Router)
 
 app.listen(5030, console.log("http://localhost:5030"));
