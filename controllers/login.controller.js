9b8a179a40838c29db99bb0984934ffa25154725
Sergio Gonzalez Barraza <114702763+SergioGzzBrz@users.noreply.github.com>
2025-03-31 12:28:24 -0600

    Add user register and login with hashing

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,17 +1,50 @@
-import { sqlConnect, sql } from '../utils/sql.js'
+import { sqlConnect, sql } from "../utils/sql.js";
+import crypto, { hash } from "crypto";
 
 export const login = async (req, res) => {
-    let isLogin = false
-    const pool = await sqlConnect();
-    const data = await pool
-        .request()
-        .input('username', sql.VarChar, req.body.username)
-        .query('SELECT * FROM users WHERE username = @username')
-    
-    isLogin = (data.recordset[0]?.password === req.body.password) 
-    if (isLogin) {
-        res.status(200).json({ isLogin: isLogin, user: data.recordset[0] })
-    } else {
-        res.status(400).json({ isLogin: isLogin, user: {} })
-    }
+  let isLogin = false;
+  const pool = await sqlConnect();
+  const data = await pool
+    .request()
+    .input("username", sql.VarChar, req.body.username)
+    .query("SELECT * FROM users WHERE username = @username");
+
+  const dbPassword = data.recordset[0]?.password;
+  const salt = dbPassword.slice(0, 10);
+
+  const pepper = process.env.PEPPER;
+  const hashing = crypto.createHash("sha512");
+
+  const hash =
+    salt +
+    hashing.update(salt + req.body.password + pepper).digest("base64url");
+
+  isLogin = data.recordset[0]?.password === hash;
+  if (isLogin) {
+    res.status(200).json({ isLogin: isLogin, user: data.recordset[0] });
+  } else {
+    res.status(400).json({ isLogin: isLogin, user: {} });
+  }
+};
+
+export const register = async (req, res) => {
+  const salt = crypto.randomBytes(24).toString().slice(0, 10);
+  const pepper = process.env.PEPPER;
+
+  const hashing = crypto.createHash("sha512");
+  const hash =
+    salt +
+    hashing.update(salt + req.body.password + pepper).digest("base64url");
+
+  const pool = await sqlConnect();
+  const data = await pool
+    .request()
+    .input("name", sql.VarChar, req.body.name)
+    .input("username", sql.VarChar, req.body.username)
+    .input("password", sql.VarChar, hash)
+    .query(
+      "INSERT INTO users(name, username, password) VALUES (@name, @username, @password)"
+    );
+
+  res.status(200).json({ done: true });
 };
